518d15d4144f241fdb5423ac03b824650e6cf067
Shigasuresh@123 <shigasuresh546@gmail.com>
2020-01-20 12:21:03 +0530

    Property Wrappers

--- PropertyWroppers/ViewController.swift
+++ PropertyWroppers/ViewController.swift
@@ -9,12 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        var user = User(firstName: "suresh kumar", lastName:"shiga")
+        print(user)
+        
+        user.lastName = "donthgaani"
+        
+        print(user)
+        
+        let document = Document()
+        print(document.name)
     }
-
-
 }
 
+
+
+
+
+@propertyWrapper struct Capitalized {
+    var wrappedValue:String {
+        didSet { wrappedValue = wrappedValue.capitalized }
+    }
+    
+    init(wrappedValue: String) {
+        self.wrappedValue = wrappedValue.capitalized
+    }
+}
+
+
+struct User {
+    @Capitalized var firstName: String
+    @Capitalized var lastName: String
+}
+
+
+struct Document {
+    @Capitalized var name = "Untitled document"
+}
